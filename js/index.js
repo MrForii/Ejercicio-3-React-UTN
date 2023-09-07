b1eab11a86d2eb0945ea9d76aaefe02eab733279
Rodrigo <fori2001@hotmail.com>
2023-09-07 20:15:36 -0300

    challenge completed

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 // Función para agregar una nueva tarea
 function agregarTarea() {
     const nuevaTareaInput = document.getElementById("nuevaTarea");
+    const nuevaTareaStatus = document.getElementById("status");
     const listaTareas = document.getElementById("listaTareas");
     const nuevaTareaTexto = nuevaTareaInput.value.trim();
 
     if (nuevaTareaTexto !== "") {
         const nuevaTarea = document.createElement("li");
         nuevaTarea.textContent = nuevaTareaTexto;
+
+        const botonCompletar = document.createElement("button");
+        botonCompletar.textContent = "Completar Tarea";
+        botonCompletar.className = "completeTask";
+        botonCompletar.onclick = function() {
+            marcarCompletada(nuevaTarea);
+        }
+
         const botonEliminar = document.createElement("button");
         botonEliminar.textContent = "Eliminar";
         botonEliminar.className = "delete";
@@ -14,7 +23,13 @@ function agregarTarea() {
             listaTareas.removeChild(nuevaTarea);
         };
 
+        if (nuevaTareaStatus.value === "completed") {
+            nuevaTarea.classList.add("completed");
+        } else {
+            nuevaTarea.classList.add("pending");
+        }
         nuevaTarea.appendChild(botonEliminar);
+        nuevaTarea.appendChild(botonCompletar);
         listaTareas.appendChild(nuevaTarea);
         nuevaTareaInput.value = "";
 
@@ -24,6 +39,7 @@ function agregarTarea() {
 function marcarCompletada(tarea) {
     tarea.classList.toggle("completed");
 }
+
 // Agregar evento de clic a las tareas para marcarlas como
 // completadas
 document.getElementById("listaTareas").addEventListener("click",
@@ -36,3 +52,36 @@ document.getElementById("listaTareas").addEventListener("click",
 // Agregar evento de clic al botón "Agregar"
 document.getElementById("agregar").addEventListener("click",
 agregarTarea);
+
+
+// Función para mostrar tareas completadas
+function mostrarCompletadas() {
+
+    const tareas = document.querySelectorAll("li");
+
+    tareas.forEach(tarea => {
+        if (tarea.classList.contains("completed")) {
+        tarea.style.display = "flex";
+        } else {
+        tarea.style.display = "none";
+    }
+    });
+    }
+
+    // Función para mostrar tareas pendientes
+    function mostrarPendientes() {
+        const tareas = document.querySelectorAll("li");
+        tareas.forEach(tarea => {
+        if (tarea.classList.contains("pending")) {
+            tarea.style.display = "flex";
+        } else {
+            tarea.style.display = "none";
+        }
+    });
+    }
+
+    // Agregar evento de clic al botón "Mostrar Completadas"
+    document.getElementById("mostrarCompletadas").addEventListener("click", mostrarCompletadas);
+
+    // Agregar evento de clic al botón "Mostrar Pendientes"
+    document.getElementById("mostrarPendientes").addEventListener("click", mostrarPendientes);
